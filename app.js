d7db2864352bb1229f5b6ac8bad421c7caa98577
ishan <ishan.khosla22@gmail.com>
2022-04-13 17:41:11 -0400

    adding final product with read me

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyEl = document.querySelector("body");
 const playerCardHolder = document.querySelector(".playerOne_hands");
 const dealerCardHolder = document.querySelector(".dealer_hands");
 const cardCountEl = document.querySelector(".cardCount");
-const dealerCardCountEl = document.querySelector('.dealerCardCount');
+const dealerCardCountEl = document.querySelector(".dealerCardCount");
 const resultEl = document.querySelector(".result");
 const tableEl = document.querySelector(".table");
 //Card Combinations
@@ -38,15 +38,24 @@ let randomDealerSymbol;
 let countTotal = 0;
 let dealerCount = 0;
 let startButton;
-let smallWindow = window.matchMedia("(max-width: 1300px)");
+let smallWindow = window.matchMedia("(max-width: 1000px)");
 
-//Sounds 
-let cardEffect = new Audio('Whoosh_Sound_Effect.mp3')
-let winnerAudio = new Audio ('winner.mp3');
+//Sounds
+let cardEffect = new Audio("Whoosh_Sound_Effect.mp3");
+let winnerAudio = new Audio("winner.mp3");
 winnerAudio.volume = 0.3;
 
 //Functions
-
+document.addEventListener("DOMContentLoaded", function () {
+  startButton = document.createElement("button");
+  startButton.classList.add("startButton");
+  startButton.innerText = "START";
+  stayBtnEl.classList.add("buttonVisibility");
+  hitBtnEl.classList.add("buttonVisibility");
+  restartBtnEl.classList.add("buttonVisibility");
+  tableEl.append(startButton);
+  startButton.addEventListener("click", gameInitiation);
+});
 
 function randomPlayerCard() {
   randomPlayerSymbol =
@@ -79,12 +88,12 @@ function dealerCards() {
     dealerCount += 10;
   } else if (randomDealerValue === "Q") {
     dealerCount += 10;
-  } else if (randomDealerValue === 'A') {
-      if (dealerCount > 10) {
-          dealerCount += 1
-      } else {
-          dealerCount += 11
-      }
+  } else if (randomDealerValue === "A") {
+    if (dealerCount > 10) {
+      dealerCount += 1;
+    } else {
+      dealerCount += 11;
+    }
   } else {
     dealerCount += parseInt(randomDealerValue);
   }
@@ -94,33 +103,37 @@ function dealerHit() {
   let dealerCard = document.createElement("div");
   dealerCards();
   cardEffect.play();
-  dealerCard.classList.add("card","cardPositioningDealer","large", 'shadow', randomDealerCombo);
+  dealerCard.classList.add(
+    "card",
+    "cardPositioningDealer",
+    "large",
+    "shadow",
+    randomDealerCombo
+  );
   if (smallWindow.matches) {
-    dealerCard.classList.remove('large');
-    dealerCard.classList.add('small');
+    dealerCard.classList.remove("large");
+    dealerCard.classList.add("small");
   }
   dealerCardHolder.append(dealerCard);
   dealerAutoHit();
 }
 
 function dealerAutoHit() {
-  hitBtnEl.classList.add('buttonVisibility');
-  stayBtnEl.classList.add('buttonVisibility');
+  hitBtnEl.classList.add("buttonVisibility");
+  stayBtnEl.classList.add("buttonVisibility");
   if (dealerCount === 21) {
     resultEl.innerHTML = "Dealer Wins!";
-    setTimeout(gameDone, 2000);
-    restartAnimation();
+    setTimeout(restartAnimation, 1000);
   } else if (dealerCount < 17) {
     setTimeout(dealerHit, 1000);
   } else if (dealerCount > 21) {
     winnerAudio.play();
     resultEl.innerHTML = "Dealer is Bust";
-    setTimeout(gameDone, 2000);
-    restartAnimation();
+    setTimeout(restartAnimation, 1000);
   } else {
     gameWinner();
   }
-  dealerCardCountEl.innerText = ("Dealer's Hand: " + dealerCount);
+  dealerCardCountEl.innerText = "Dealer's Hand: " + dealerCount;
 }
 
 function cardAce() {
@@ -131,16 +144,16 @@ function cardAce() {
   aceCard1.classList.add("aceOptionBtn");
   aceCard11.classList.add("aceOptionBtn11");
   playerCardHolder.append(aceCard1, aceCard11);
-  hitBtnEl.classList.add('buttonVisibility');
-  stayBtnEl.classList.add('buttonVisibility');
+  hitBtnEl.classList.add("buttonVisibility");
+  stayBtnEl.classList.add("buttonVisibility");
 
   aceCard1.addEventListener("click", function () {
     countTotal++;
     playerCount();
     aceCard1.remove();
     aceCard11.remove();
-    hitBtnEl.classList.remove('buttonVisibility');
-  stayBtnEl.classList.remove('buttonVisibility');
+    hitBtnEl.classList.remove("buttonVisibility");
+    stayBtnEl.classList.remove("buttonVisibility");
   });
 
   aceCard11.addEventListener("click", function () {
@@ -148,8 +161,8 @@ function cardAce() {
     playerCount();
     aceCard1.remove();
     aceCard11.remove();
-    hitBtnEl.classList.remove('buttonVisibility');
-  stayBtnEl.classList.remove('buttonVisibility');
+    hitBtnEl.classList.remove("buttonVisibility");
+    stayBtnEl.classList.remove("buttonVisibility");
   });
 }
 
@@ -157,106 +170,101 @@ function playerCount() {
   if (countTotal === 21) {
     winnerAudio.play();
     resultEl.innerHTML = "WINNER!";
-    setTimeout(gameDone, 2000);
-    restartAnimation();
+    setTimeout(restartAnimation, 1000);
   } else if (countTotal > 21) {
     resultEl.innerHTML = "Bust, Dealer Wins!";
-    setTimeout(gameDone, 2000);
-    restartAnimation();
+    setTimeout(restartAnimation, 1000);
   } else {
   }
-  cardCountEl.innerHTML = ("Player's Hand: " + countTotal);
+  cardCountEl.innerHTML = "Player's Hand: " + countTotal;
 }
 
 function gameWinner() {
   if (countTotal > dealerCount) {
     winnerAudio.play();
     resultEl.innerHTML = "Player Wins!";
-    setTimeout(gameDone, 2000);
-    restartAnimation();
+    setTimeout(restartAnimation, 1000);
   } else if (dealerCount > countTotal) {
     resultEl.innerHTML = "Dealer Wins!";
-    setTimeout(gameDone, 2000);
-    restartAnimation();
+    setTimeout(restartAnimation, 1000);
   } else {
     resultEl.innerHTML = "DRAW";
-    setTimeout(gameDone, 2000);
-    restartAnimation();
+    setTimeout(restartAnimation, 1000);
   }
 }
 
- //restart function FIX
-    function restart () {
-        resultEl.innerText = '';
-        cardCountEl.innerText = '';
-        playerCardHolder.innerHTML = '';
-        restartBtnEl.classList.remove('restartAnimation');
-        hitBtnEl.classList.remove('buttonVisibility');
-        stayBtnEl.classList.remove('buttonVisibility');
-        gameInitiation();     
-    }
-
 function gameDone() {
-    cardCountEl.innerHTML = "";
-    playerCardHolder.innerHTML='';
-    dealerCardHolder.innerHTML='';
-    dealerCardCountEl.innerText='';
-    stayBtnEl.classList.add('buttonVisibility');
-    hitBtnEl.classList.add('buttonVisibility');
-    countTotal = 0;
-    dealerCount = 0;
+  resultEl.innerText = "";
+  cardCountEl.innerHTML = "";
+  playerCardHolder.innerHTML = "";
+  dealerCardHolder.innerHTML = "";
+  dealerCardCountEl.innerText = "";
+  stayBtnEl.classList.add("buttonVisibility");
+  hitBtnEl.classList.add("buttonVisibility");
+  countTotal = 0;
+  dealerCount = 0;
 }
 
-// function restartAnimation () {
-//   restartBtnEl.classList.remove('buttonVisibility');
-//   restartBtnEl.classList.add('restartAnimation');
-//   hitBtnEl.classList.add('buttonVisibility');
-//   stayBtnEl.classList.add('buttonVisibility');
-// }
-
-function gameInitiation () {
-  startButton.classList.add('buttonVisibility');
-  stayBtnEl.classList.add('buttonVisibility');
-  hitBtnEl.classList.add('buttonVisibility');
+function gameInitiation() {
+  startButton.classList.add("buttonVisibility");
+  stayBtnEl.classList.add("buttonVisibility");
+  hitBtnEl.classList.add("buttonVisibility");
   let card = document.createElement("div");
   randomPlayerCard();
   cardEffect.play();
-  card.classList.add("card","cardPositioningP1","large",'shadow',randomPlayerCombination);
+  card.classList.add(
+    "card",
+    "cardPositioningP1",
+    "large",
+    "shadow",
+    randomPlayerCombination
+  );
   if (smallWindow.matches) {
-    card.classList.remove('large');
-    card.classList.add('small');
+    card.classList.remove("large");
+    card.classList.add("small");
   }
   playerCardHolder.append(card);
+  console.log(card);
   playerCount();
-  setTimeout(function () { 
-  let card2 = document.createElement("div");
-  randomPlayerCard();
-  cardEffect.play();
-  card2.classList.add("card","cardPositioningP1","large",'shadow',randomPlayerCombination);
-  if (smallWindow.matches) {
-    card2.classList.remove('large');
-    card2.classList.add('small');
-  }
-  playerCardHolder.append(card2);
-  playerCount();
-  stayBtnEl.classList.remove('buttonVisibility');
-  hitBtnEl.classList.remove('buttonVisibility');}, 1000) 
-  
-  }
-  
-  
-
+  setTimeout(function () {
+    let card2 = document.createElement("div");
+    randomPlayerCard();
+    cardEffect.play();
+    card2.classList.add(
+      "card",
+      "cardPositioningP1",
+      "large",
+      "shadow",
+      randomPlayerCombination
+    );
+    if (smallWindow.matches) {
+      card2.classList.remove("large");
+      card2.classList.add("small");
+    }
+    playerCardHolder.append(card2);
+    console.log(card2);
+    playerCount();
+    stayBtnEl.classList.remove("buttonVisibility");
+    hitBtnEl.classList.remove("buttonVisibility");
+  }, 1200);
+}
 
 //EVENT LISTENERS
 hitBtnEl.addEventListener("click", function () {
-  stayBtnEl.classList.remove('buttonVisibility');
+  stayBtnEl.classList.remove("buttonVisibility");
   let card = document.createElement("div");
   randomPlayerCard();
   cardEffect.play();
-  card.classList.add("card","cardPositioningP1","large",'shadow',randomPlayerCombination);
+  card.classList.add(
+    "card",
+    "cardPositioningP1",
+    "large",
+    "shadow",
+    randomPlayerCombination
+  );
   if (smallWindow.matches) {
-    card.classList.remove('large');
-    card.classList.add('small');
+    card.classList.remove("large");
+    card.classList.add("small");
   }
   playerCardHolder.append(card);
   playerCount();
@@ -264,16 +272,17 @@ hitBtnEl.addEventListener("click", function () {
 
 stayBtnEl.addEventListener("click", dealerHit);
 
-restartBtnEl.addEventListener('click', restart);
+restartBtnEl.addEventListener("click", function () {
+  gameDone();
+  gameInitiation();
+  restartBtnEl.classList.add("buttonVisibility");
+});
 
-document.addEventListener('DOMContentLoaded', function () {
-  startButton = document.createElement('button');
-  startButton.classList.add('startButton');
-  startButton.innerText = ('START');
-  stayBtnEl.classList.add('buttonVisibility');
-  hitBtnEl.classList.add('buttonVisibility');
-  tableEl.append(startButton);
-  startButton.addEventListener('click',gameInitiation);
+// restartBtnEl.classList.remove('restartAnimation');
 
-})
-
+function restartAnimation() {
+  hitBtnEl.classList.add("buttonVisibility");
+  stayBtnEl.classList.add("buttonVisibility");
+  restartBtnEl.classList.remove("buttonVisibility");
+  restartBtnEl.classList.add("restartAnimation");
+}
